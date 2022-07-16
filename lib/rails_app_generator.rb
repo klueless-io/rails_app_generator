574d1bf9aa90ed157cdb38c02e771883be9af3a0
David Cruwys <david@ideasmen.com.au>
2022-07-16 20:07:00 +1000

    first commit

---
+++ lib/rails_app_generator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rails_app_generator/version"
+
+module RailsAppGenerator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rails_app_generator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RailsAppGenerator do
+  it "has a version number" do
+    expect(RailsAppGenerator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
