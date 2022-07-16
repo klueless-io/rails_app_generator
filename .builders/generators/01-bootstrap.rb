44950f8a50d731ae987b8c136c702f1eea282274
David Cruwys <david@ideasmen.com.au>
2022-07-16 20:08:38 +1000

    chore: set up the package.json file for semantic versioning

--- .builders/generators/01-bootstrap.rb
+++ .builders/generators/01-bootstrap.rb
@@ -33,7 +33,7 @@ KManager.action :bootstrap do
         # run_command('git init')
       end
       .blueprint(
-        active: true,
+        active: false,
         name: :bin_hook,
         description: 'initialize repository',
         on_exist: :write) do
@@ -53,7 +53,7 @@ KManager.action :bootstrap do
         run_command("gh repo edit -d \"#{dom[:application_description]}\"")
       end
       .package_json(
-        active: false,
+        active: true,
         name: :package_json,
         description: 'Set up the package.json file for semantic versioning'
       ) do
