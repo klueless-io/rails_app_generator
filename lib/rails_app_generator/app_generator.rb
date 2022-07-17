fe3d97744e3313a2769f483868d94f1e3b3131da
David Cruwys <david@ideasmen.com.au>
2022-07-17 11:16:37 +1000

    add app_generator

--- lib/rails_app_generator/app_generator.rb
+++ lib/rails_app_generator/app_generator.rb
@@ -3,14 +3,19 @@
 require 'rails/generators/rails/app/app_generator'
 
 module RailsAppGenerator
-  class AppGenerator < Rails::Generators::AppGenerator
+  #  < Rails::Generators::AppGenerator
+  class AppGenerator
     attr_reader :output_folder
 
-    def initialize(*args) #@, **opts)
-      # @output_folder = opts[:output_folder]
-      super(*args) # , **opts)
-
-      gem_path = Gem.loaded_specs["railties"].full_gem_path
+    def initialize(**args)
+      @output_folder = args[:output_folder] || Dir.pwd
     end
+
+    # def initialize(*args, **opts) #@, **opts)
+    #   # @output_folder = opts[:output_folder]
+    #   super(*args) # , **opts)
+
+    #   gem_path = Gem.loaded_specs["railties"].full_gem_path
+    # end
   end
 end
--- spec/lib/rails_app_generator/app_generator_spec.rb
+++ spec/lib/rails_app_generator/app_generator_spec.rb
@@ -1,17 +1,28 @@
 # frozen_string_literal: true
 
 RSpec.describe RailsAppGenerator::AppGenerator do
-  let(:instance) { described_class.new }#(**args) }
+  let(:instance) { described_class.new(**args) }
   let(:args) { { output_folder: '~/dev/kweb/xmen' } }
   # subject { instance.name }
 
-  it {
-    gem_path = Gem.loaded_specs["railties"].full_gem_path
-    # lib/rails/generators/rails/app/templates
-    templates_root = File.expand_path(File.join(gem_path, "lib/rails/generators/rails/app/templates"))
+  # it {
+  #   gem_path = Gem.loaded_specs["railties"].full_gem_path
+  #   # lib/rails/generators/rails/app/templates
+  #   templates_root = File.expand_path(File.join(gem_path, "lib/rails/generators/rails/app/templates"))
 
-    described_class.source_root templates_root
+  #   described_class.source_root templates_root
+  #   # destination_root = "/Users/davidcruwys/dev/kweb/xmen"
+  #   destination_root = "/Users/davidcruwys/dev/kgems/rails_app_generator/a/sample1"
+  #   described_class.start [destination_root, '--skip-bundle']
+  # }
 
-    described_class.start ['x', '--skip-bundle']
-  }
+  describe 'initialize' do
+    let(:args) { {} }
+
+    describe '.output_folder' do
+      subject { instance.output_folder }
+
+      it { is_expected.to eq(Dir.pwd) }
+    end
+  end
 end
