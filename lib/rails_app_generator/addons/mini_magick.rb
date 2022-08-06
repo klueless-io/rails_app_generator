f44b932df8fa7d817e2064d74af49476f82710d3
David Cruwys <david@ideasmen.com.au>
2022-08-06 23:58:02 +1000

    fix: profile mini_magick

--- lib/rails_app_generator/addons/mini_magick.rb
+++ lib/rails_app_generator/addons/mini_magick.rb
@@ -7,8 +7,7 @@ module RailsAppGenerator
     class MiniMagick < AddOn
       required_gem gem.version('mini_magick', '4.11.0', 'Manipulate images with minimal use of memory via ImageMagick / GraphicsMagick')
 
-      def apply
-      end
+      def apply; end
     end
   end
 end
