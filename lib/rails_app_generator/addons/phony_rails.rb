77aec0e7ed2fe9093cb988ab408db1f1e50e78d3
David Cruwys <david@ideasmen.com.au>
2022-08-11 09:03:26 +1000

    fix: add phony_rails addon

--- lib/rails_app_generator/addons/phony_rails.rb
+++ lib/rails_app_generator/addons/phony_rails.rb
@@ -7,8 +7,7 @@ module RailsAppGenerator
     class PhonyRails < RailsAppGenerator::Addon
       required_gem gem.version('phony_rails', '0.15.0', 'This Gem adds useful methods to your Rails app to validate, display and save phone numbers.')
 
-      def apply
-      end
+      def apply; end
     end
   end
 end
--- lib/rails_app_generator/addons/public_suffix.rb
+++ lib/rails_app_generator/addons/public_suffix.rb
@@ -7,8 +7,7 @@ module RailsAppGenerator
     class PublicSuffix < RailsAppGenerator::Addon
       required_gem gem.version('public_suffix', '5.0.0', 'PublicSuffix can parse and decompose a domain name into top level domain, domain and subdomains.')
 
-      def apply
-      end
+      def apply; end
     end
   end
 end
