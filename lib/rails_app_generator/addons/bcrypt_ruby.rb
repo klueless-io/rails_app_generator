4622249b03ab204728432d04d4743e8ba8569ec3
David Cruwys <david@ideasmen.com.au>
2022-08-11 14:20:56 +1000

    fix: add bcrypt_ruby addon

---
+++ lib/rails_app_generator/addons/bcrypt_ruby.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module RailsAppGenerator
+  # Custom add-ons for RailsAppGenerator
+  module AddOns
+    # Add BcryptRuby to rails application
+    class BcryptRuby < RailsAppGenerator::Addon
+      required_gem gem.version('bcrypt-ruby', '3.1.5', 'bcrypt() is a sophisticated and secure hash algorithm designed by The OpenBSD project for hashing passwords.')
+
+      def apply
+      end
+    end
+  end
+end
